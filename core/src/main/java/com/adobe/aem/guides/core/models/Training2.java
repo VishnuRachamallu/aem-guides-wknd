d1717f1cc4ae73c6bcd86842c4cb3bea2e5a4162
Vishnu Rachamallu <vitturachamallu001@gmail.com>
2022-10-11 08:27:21 +0530

    second commit

--- core/src/main/java/com/adobe/aem/guides/core/models/Training2.java
+++ core/src/main/java/com/adobe/aem/guides/core/models/Training2.java
@@ -51,8 +51,7 @@ import java.util.Optional;
 public class Training2 implements Training2Inte {
 
 	public static final Logger LOG=org.slf4j.LoggerFactory.getLogger(Training2.class);
-	int abc=124;
-	
+
 	@SlingObject
 	ResourceResolver resourceResolver;
 	
